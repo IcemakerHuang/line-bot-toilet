3af7dc14c6c05a2d005ddd2b8e1b486d75c5da4c
IcemakerHuang <huang830226@gmail.com>
2023-11-28 16:38:17 +0800

    測試-3

--- commands/toilet.js
+++ commands/toilet.js
@@ -3,62 +3,65 @@ import replyTemplate from '../templates/replies.js'
 import { distance } from './distance.js'
 
 export default async (event) => {
-	try {
-		// 抓自己的位置
-		// 算距離 符合某一個條件
-		const { data } = await axios.get('../data/Data_TPE+NTPC(format).json')
-		const msg = []
-		data
-			.map((value) => {
-				value.distance = distance(
-					event.message.latitude,
-					event.message.longitude,
-					data[0].latitude,
-					data[0].longitude,
-					'K'
-				)
-				return value
-			})
-			.filter((value) => {
-				return value.distance < 3
-			})
-			.sort((a, b) => {
-				return a.distance - b.distance
-			})
-			.slice(0, 3)
-			.forEach((value) => {
-				const msg = replyTemplate()
-				msg.hero.url = 'https://img.ltn.com.tw/Upload/news/600/2022/04/19/3898832_1_1.jpg'
-				msg.body.contents[0].text = value.name
-				msg.push(msg)
-				console.log(value)
-			})
+  try {
+    console.log('AAAAAA')
+    // 抓自己的位置
+    // 算距離 符合某一個條件
+    const { data } = await axios.get('../data/Data_TPE+NTPC(format).json')
+    const box = []
+    data
+      .map((value) => {
+        value.distance = distance(
+          event.message.latitude,
+          event.message.longitude,
+          data[0].latitude,
+          data[0].longitude,
+          'K'
+        )
+        console.log(event.message.latitude)
+        console.log(value)
+        return value
+      })
+      .filter((value) => {
+        return value.distance < 10
+      })
+      .sort((a, b) => {
+        return a.distance - b.distance
+      })
+      .slice(0, 3)
+      .forEach((value) => {
+        const msg = replyTemplate()
+        msg.hero.url = 'https://img.ltn.com.tw/Upload/news/600/2022/04/19/3898832_1_1.jpg'
+        msg.body.contents[0].text = value.name
+        box.push(msg)
+        console.log(value)
+      })
 
-		const result = await event.reply({
-			type: 'flex',
-			altText: '查詢結果',
-			contents: {
-				type: 'carousel',
-				contents: 'temples'
-			}
-		})
+    const result = await event.reply({
+      type: 'flex',
+      altText: '查詢結果',
+      contents: {
+        type: 'carousel',
+        contents: 'temples'
+      }
+    })
+    console.log(result)
+    // for (let i = 0; i < data.length; i++) {
+    //   const lat1 = event.message.latitude
+    //   const lon1 = event.message.longitude
+    //   const lat2 = data[0].latitude
+    //   const lon2 = data[0].longitude
 
-		// for (let i = 0; i < data.length; i++) {
-		//   const lat1 = event.message.latitude
-		//   const lon1 = event.message.longitude
-		//   const lat2 = data[0].latitude
-		//   const lon2 = data[0].longitude
+    //   const dist = distance(lat1, lon1, lat2, lon2, 'K')
+    //   if (dist < 3) {
+    //     console.log(data[i])
+    //     msg.push(data[i])
+    //   }
+    // }
 
-		//   const dist = distance(lat1, lon1, lat2, lon2, 'K')
-		//   if (dist < 3) {
-		//     console.log(data[i])
-		//     msg.push(data[i])
-		//   }
-		// }
-
-		// const template = replyTemplate()
-		// template.body.contents[0].contents[0].text = msg.data[]
-	} catch (error) {
-		console.log(error)
-	}
+    // const template = replyTemplate()
+    // template.body.contents[0].contents[0].text = msg.data[]
+  } catch (error) {
+    console.log(error)
+  }
 }
--- index.js
+++ index.js
@@ -5,104 +5,104 @@ import be from './commands/be.js'
 import anime from './commands/anime.js'
 import { scheduleJob } from 'node-schedule'
 import * as usdtwd from './data/usdtwd.js'
-import toilet from './commands/toilet.js'
+// import toilet from './commands/toilet.js'
 
 // https://crontab.guru/once-a-day
 scheduleJob('0 0 * * *', () => {
-	usdtwd.update()
+  usdtwd.update()
 })
 usdtwd.update()
 
 // 基本機器人架構
 // 之後可以把帳號資訊放進.env
 const bot = linebot({
-	channelId: process.env.CHANNEL_ID,
-	channelSecret: process.env.CHANNEL_SECRET,
-	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
 // 當收到訊息時執行一個fn
 // 把太多指令拆掉，變成fe，再import呼叫指令回來
 bot.on('message', (event) => {
-	if (process.env.DEBUG === 'true') {
-		console.log(event)
-	}
-	// console.log(123)
-	if (event.message.type === 'text') {
-		if (event.message.text === '前端') {
-			fe(event)
-		} else if (event.message.text === '後端') {
-			// console.log(456)
-			be(event)
-		} else if (event.message.text.startsWith('動畫')) {
-			// 動畫14882
-			anime(event)
-		} else if (event.message.text === '匯率') {
-			event.reply(usdtwd.exrate.toString())
-		} else if (event.message.text === '123') {
-			event.reply({
-				type: 'text',
-				text: '123',
-				quickReply: {
-					items: [
-						{
-							type: 'action',
-							action: {
-								// 傳訊息
-								type: 'message',
-								// 傳送的文字
-								text: 'message text',
-								// 按鈕的文字
-								label: 'message'
-							}
-						},
-						{
-							type: 'action',
-							action: {
-								type: 'cameraRoll',
-								label: '相簿'
-							}
-						},
-						{
-							type: 'action',
-							action: {
-								type: 'cameraRoll',
-								label: '位置'
-							}
-						},
-						{
-							type: 'action',
-							action: {
-								type: 'uri',
-								uri: 'https://wdaweb.github.io',
-								label: '網址'
-							}
-						},
-						{
-							type: 'action',
-							action: {
-								type: 'postback',
-								label: 'postback',
-								// 傳送的文字
-								// text: 'postback 文字'
-								// postback 事件接收到的資訊
-								data: '111222333'
-							}
-						}
-					]
-				}
-			})
-		} else if (event.message.text === 'location') {
-			toilet(event)
-		}
-	}
+  if (process.env.DEBUG === 'true') {
+    console.log(event)
+  }
+  // console.log(123)
+  if (event.message.type === 'text') {
+    if (event.message.text === '前端') {
+      fe(event)
+    } else if (event.message.text === '後端') {
+      // console.log(456)
+      be(event)
+    } else if (event.message.text.startsWith('動畫')) {
+      // 動畫14882
+      anime(event)
+    } else if (event.message.text === '匯率') {
+      event.reply(usdtwd.exrate.toString())
+    } else if (event.message.text === '123') {
+      event.reply({
+        type: 'text',
+        text: '123',
+        quickReply: {
+          items: [
+            {
+              type: 'action',
+              action: {
+                // 傳訊息
+                type: 'message',
+                // 傳送的文字
+                text: 'message text',
+                // 按鈕的文字
+                label: 'message'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'cameraRoll',
+                label: '相簿'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'cameraRoll',
+                label: '位置'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'uri',
+                uri: 'https://wdaweb.github.io',
+                label: '網址'
+              }
+            },
+            {
+              type: 'action',
+              action: {
+                type: 'postback',
+                label: 'postback',
+                // 傳送的文字
+                // text: 'postback 文字'
+                // postback 事件接收到的資訊
+                data: '111222333'
+              }
+            }
+          ]
+        }
+      })
+    } else if (event.message.type === 'location') {
+      fe(event)
+    }
+  }
 })
 bot.on('postback', (event) => {
-	console.log(event.postback.data)
+  console.log(event.postback.data)
 })
 
 bot.listen('/', process.env.PORT || 3000, () => {
-	console.log('機器人啟動')
+  console.log('機器人啟動')
 })
 // 上面'/'代表 賴裡面 Webhook URL 後面網址要接上什麼。
 // 連接埠 3000、可見度公開、轉送位置貼webhook URL
