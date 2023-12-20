e201fc430cae77dadc846ebb0e89fb54ec2c6623
IcemakerHuang <huang830226@gmail.com>
2023-12-20 16:36:34 +0800

    處理位置問題

--- commands/toilet.js
+++ commands/toilet.js
@@ -7,17 +7,14 @@ export default async (event) => {
     console.log('AAAAAA')
     // 抓自己的位置
     // 算距離 符合某一個條件
-    const { data } = await axios.get('https://raw.githubusercontent.com/IcemakerHuang/Json/main/data/Data_TPE%2BNTPC.json')
+    const { data } = await axios.get(
+      'https://raw.githubusercontent.com/IcemakerHuang/Json/main/data/Data_TPE%2BNTPC.json'
+    )
     const box = []
     data
       .map((value) => {
-        value.distance = distance(
-          event.message.latitude,
-          event.message.longitude,
-          value.latitude,
-          value.longitude,
-          'K'
-        )
+        // 取經緯度
+        value.distance = distance(event.message.latitude, event.message.longitude, value.latitude, value.longitude, 'K')
         console.log(event.message.latitude)
         console.log(value)
         return value
@@ -30,10 +27,11 @@ export default async (event) => {
       })
       .slice(0, 3)
       .forEach((value) => {
-        const msg = replyTemplate()
-        msg.contents[0].hero.url = 'https://img.ltn.com.tw/Upload/news/600/2022/04/19/3898832_1_1.jpg'
-        msg.contents[0].body.contents[0].text = value.name
-        box.push(msg)
+        const template = replyTemplate()
+        const bubble = template.contents[0] // 获取 replyTemplate 中的第一个 bubble
+        bubble.hero.url = 'https://img.ltn.com.tw/Upload/news/600/2022/04/19/3898832_1_1.jpg'
+        bubble.body.contents[0].text = value.name
+        box.push(bubble) // 将这个 bubble 添加到 box 数组
         console.log(value)
         // const msg = replyTemplate()
         // msg.hero.url = 'https://img.ltn.com.tw/Upload/news/600/2022/04/19/3898832_1_1.jpg'
@@ -55,6 +53,7 @@ export default async (event) => {
     console.log(error)
   }
 }
+
 // import fs from 'fs'
 // import util from 'util'
 // import replyTemplate from '../templates/replies.js'
@@ -69,22 +68,17 @@ export default async (event) => {
 //     const data = JSON.parse(dataString)
 //     const box = []
 
-//     data.map((value) => {
-//       value.distance = distance(
-//         event.message.latitude,
-//         event.message.longitude,
-//         value.latitude,
-//         value.longitude,
-//         'K'
-//       )
-//       console.log(event.message.latitude)
-//       console.log(value)
-//       return value
-//     })
+//     data
+//       .map((value) => {
+//         value.distance = distance(event.message.latitude, event.message.longitude, value.latitude, value.longitude, 'K')
+//         console.log(event.message.latitude)
+//         console.log(value)
+//         return value
+//       })
 //       .filter((value) => {
 //         return value.distance < 10
 //       })
-//       .sort((a, b) => {
+//       .sort((a, b) => {+
 //         return a.distance - b.distance
 //       })
 //       .slice(0, 3)
